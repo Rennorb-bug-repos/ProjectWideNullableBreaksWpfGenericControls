96beb413584d7ce12f9a3579e63da9d66fc826a1
Rennorb <lplucas99@gmail.com>
2022-08-19 16:08:15 +0200

    Add project files.

---
+++ TestControl.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+
+namespace ProjectWideNullWithGenericControlls {
+	public partial class TestControl : Generic<TypeArgument> {
+		public TestControl()
+		{
+			InitializeComponent();
+		}
+	}
+
+	public class TypeArgument {}
+	public class Generic<T> : Base where T : class {}
+	public class Base : UserControl {}
+}
